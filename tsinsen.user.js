1877045242e3c7dc309231672ceacb4a4fdd0ce8
lvkai <lvkai79@gmail.com>
2021-06-09 00:10:44 +0800

    统计得分

--- tsinsen.user.js
+++ tsinsen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         清澄作业统计
 // @namespace    tsinsen
-// @version      0.3
+// @version      0.4
 // @description  统计作业完成情况
 // @author       lvkai
 // @match        http://sy.hhwdd.com/new/user/myassignments.page
@@ -13,45 +13,49 @@
     'use strict';
 
 setTimeout(function(){
+    var part = 0;
     //全部题
-    var all = new Array();
+    let all = new Set();
     $(".btn.btn-prob").each(function (index, element) {
-        all.push($(this).attr("gpid"));//往数组中存入值
+        all.add($(this).attr("gpid"));
     });
-    $.unique(all.sort());
-    var a = all.length;
 
     //未做题
-    var undo = new Array();
+    let undo = new Set();
     $(".btn-default.btn-prob").each(function (index, element) {
-        undo.push($(this).attr("gpid"));//往数组中存入值
+        undo.add($(this).attr("gpid"));
     });
-    $.unique(undo.sort());
-    var b = undo.length;
 
     //低分题
-    var low = new Array();
+    let low = new Set();
     $(".btn-warning.btn-prob").each(function (index, element) {
-        low.push($(this).attr("gpid"));//往数组中存入值
+        var gpid = $(this).attr("gpid");
+        if(!low.has(gpid)){
+            var p1 = $(this).text().split("\(");
+            var p2 = p1[p1.length - 1].split("\)");
+            part += p2[0] * 1;
+        }
+        low.add(gpid);
     });
-    $.unique(low.sort());
-    var c = low.length;
 
     //高分题
-    var high = new Array();
+    let high = new Set();
     $(".btn-info.btn-prob").each(function (index, element) {
-        high.push($(this).attr("gpid"));//往数组中存入值
+        var gpid = $(this).attr("gpid");
+        if(!high.has(gpid)){
+            var p1 = $(this).text().split("\(");
+            var p2 = p1[p1.length - 1].split("\)");
+            part += p2[0] * 1;
+        }
+        high.add(gpid);
     });
-    $.unique(high.sort());
-    var d = high.length;
 
     //满分题
-    var good = new Array();
+    let good = new Set();
     $(".btn-success.btn-prob").each(function (index, element) {
-        good.push($(this).attr("gpid"));//往数组中存入值
+        var gpid = $(this).attr("gpid");
+        good.add(gpid);
     });
-    $.unique(good.sort());
-    var e = good.length;
 
     //更新页面;
     var abc1 = $('<div id="abc1" style="color:black;font-size:14px;font-weight:normal"></div>');
@@ -60,6 +64,6 @@ setTimeout(function(){
     $("#assignmenttitle").after(abc1);
 
     document.getElementById("abc1").innerHTML="&nbsp;&nbsp;共" + ($(".prlist-container.well").length - $("span:contains('自由练习')").length) + "次课，" + $( "span:contains('自由练习')" ).length + "次练习，已完成" + $("span:contains('已完成')").length + "课。";
-    document.getElementById("abc2").innerHTML="&nbsp;&nbsp;共" + a + "题，未做" + b + "题，低分" + c + "题，高分" + d + "题，满分" + e + "题。";
+    document.getElementById("abc2").innerHTML="&nbsp;&nbsp;共" + all.size + "题，未做" + undo.size + "题，低分" + low.size + "题，高分" + high.size + "题，满分" + good.size + "题。总分" + (good.size * 100 + part).toLocaleString() + "分.";
 }, 800)
 })();
