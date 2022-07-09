f0c1cfc31172990b4df3193b5429f45dd81a39be
lvkai <lvkai79@gmail.com>
2022-07-09 15:55:25 +0800

    支持分页汇总

--- tsinsen.user.js
+++ tsinsen.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         清澄作业统计
 // @namespace    tsinsen
-// @version      0.7
+// @version      0.8
 // @description  统计作业完成情况
 // @author       lvkai
 // @match        https://sy.hhwdd.com/new/user/myassignments.page
@@ -12,59 +12,72 @@
 (function() {
     'use strict';
 
-setTimeout(function(){
-    let part = 0;
     //全部题
     let all = new Set();
-    $(".btn.btn-prob").each(function (index, element) {
-        all.add($(this).attr("gpid"));
-    });
-
     //未做题
     let undo = new Set();
-    $(".btn-default.btn-prob").each(function (index, element) {
-        undo.add($(this).attr("gpid"));
-    });
-
     //低分题
     let low = new Set();
-    $(".btn-warning.btn-prob").each(function (index, element) {
-        let gpid = $(this).attr("gpid");
-        if(!low.has(gpid)){
-            let p1 = $(this).text().split("\(");
-            let p2 = p1[p1.length - 1].split("\)");
-            part += p2[0] * 1;
-        }
-        low.add(gpid);
-    });
-
     //高分题
     let high = new Set();
-    $(".btn-info.btn-prob").each(function (index, element) {
-        let gpid = $(this).attr("gpid");
-        if(!high.has(gpid)){
-            let p1 = $(this).text().split("\(");
-            let p2 = p1[p1.length - 1].split("\)");
-            part += p2[0] * 1;
-        }
-        high.add(gpid);
-    });
-
     //满分题
     let good = new Set();
-    $(".btn-success.btn-prob").each(function (index, element) {
-        good.add($(this).attr("gpid"));
-    });
+    //总分
+    let p = 0;
+    //课数
+    let lesson = 0;
+    let finish = 0;
 
-    let p = good.size * 100 + part;
+    let page = 1;
+    let pagecount = 12;
+    while(page <= pagecount){
+        $.getJSON( "assignment.MyAssignments.dt?alldomain=yes&page=" + page, function(data ) {
+            //遍历页
+            $.each(data, function(ke, ke_val) {
+                if(!isNaN(ke)) {
+                    //遍历课
+                    lesson++;
+                    let lessonpass = true;
+                    $.each(ke_val, function(ti, ti_val) {
+                        if(!isNaN(ti)) {
+                            //遍历题的属性
+                            let gpid = "";
+                            let score = 0;
+                            $.each(ti_val, function(k, v) {
+                                if(k == "gpid") gpid = v;
+                                if(k == "score"){
+                                    if(!isNaN(v)) score = v * 1;
+                                    if(score != 100) lessonpass = false;
+                                }
+                            });
+                            //如果不是重复题
+                            if(!all.has(gpid)){
+                                all.add(gpid);
+                                if(score == 100) good.add(gpid);
+                                else if(score >= 60) high.add(gpid);
+                                else if(score > 0) low.add(gpid);
+                                else if(score == 0) undo.add(gpid);
+                                p += score;
+                            }
+                        }
+                    });
+                    if(lessonpass){
+                        finish++;
+                    }
+                }
+            });
+        });
+        page++;
+    }
 
+setTimeout(function(){
     //更新页面;
     let abc1 = $('<div id="abc1" style="color:black;font-size:14px;font-weight:normal"></div>');
     let abc2 = $('<div id="abc2" style="color:black;font-size:14px;font-weight:normal"></div>');
     $("#assignmenttitle").after(abc2);
     $("#assignmenttitle").after(abc1);
 
-    document.getElementById("abc1").innerHTML="&nbsp;&nbsp;共" + $(".prlist-container.well").length + "次课，已完成" + $("span:contains('已完成')").length + "课。";
+    document.getElementById("abc1").innerHTML="&nbsp;&nbsp;共" + lesson + "课，已完成" + finish + "课。";
     document.getElementById("abc2").innerHTML="&nbsp;&nbsp;共" + all.size + "题，未做" + undo.size + "题，已做" + (all.size - undo.size) + "题（低分" + low.size + "题，高分" + high.size + "题，满分" + good.size + "题）。总分" + p.toLocaleString() + "分。";
 }, 800)
 })();
