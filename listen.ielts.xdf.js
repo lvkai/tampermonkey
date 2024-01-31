8882ec2330cd7f6a1b2321e7c41db48502504f72
lvkai <lvkai79@gmail.com>
2024-01-31 20:42:51 +0800

    Update listen.ielts.xdf.js

--- listen.ielts.xdf.js
+++ listen.ielts.xdf.js
@@ -16,15 +16,19 @@
     setTimeout(function(){
         let abc1 = $('<div style="float:left;font-size:18px;"><br/>听写:</div><div></div>&nbsp;<div><textarea id="tb" style="resize:height;float:center;width:95%;font-size:20px;" rows="12"></textarea></div>');
         $(".boxParent").after(abc1);
-        $(".ShortcutKey").children().first().text('快捷键：“Esc” 播放/暂停');
+        $(".ShortcutKey").children().first().text('快捷键：“Ctrl” 播放/暂停');
         $("#tb").val(localStorage.getItem('tb'));
 
         $("#tb").keydown(function(event){
             if(event.keyCode == 32){
                 event.stopPropagation();
-            }else if(event.keyCode == 27){
+            }
+        });
+        $(document).keydown(function(event){
+            if(event.keyCode == 17){
                 $("#cenPlay").click();
             }
+            return true;
         });
         $("#tb").keyup(function(event){
             localStorage.setItem('tb', $("#tb").val());
