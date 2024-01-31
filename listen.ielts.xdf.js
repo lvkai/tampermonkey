36ed79233b592600cffc9412ff423b05f74b6c8e
lvkai <lvkai79@gmail.com>
2024-01-31 14:15:25 +0800

    Update listen.ielts.xdf.js

--- listen.ielts.xdf.js
+++ listen.ielts.xdf.js
@@ -2,8 +2,8 @@
 // @name         新东方雅思精听
 // @namespace    http://tampermonkey.net/
 // @version      2024-01-31
-// @description  add a text area for input when you listening
-// @author       lvkai
+// @description  try to take over the world!
+// @author       You
 // @match        https://ieltscat.xdf.cn/intensive/intensive/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=xdf.cn
 // @grant        none
@@ -14,7 +14,7 @@
     'use strict';
 
     setTimeout(function(){
-        let abc1 = $('<div>&nbsp;</div><div><textarea id= "tb" style="resize:none;float:center;width:95%;height:200px;font-size:20px;"></textarea></div>');
+        let abc1 = $('<div>&nbsp;</div><div><textarea id= "tb" style="resize:height;float:center;width:95%;font-size:20px;" rows="12"></textarea></div>');
         $(".boxParent").after(abc1);
 
         $("#tb").keydown(function(event){
